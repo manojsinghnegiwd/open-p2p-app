4d6f7c1a2de12cb5a84cc11013d8b18dd41ba1c7
Manoj Singh Negi <justanothermanoj@gmail.com>
2021-05-25 19:59:31 +0530

    added peerjs to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import PeerJS from 'peerjs';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const peer = new PeerJS(new Date().getTime().toString(), {
+      host: 'localhost',
+      port: 9000,
+      path: '/peer'
+    });
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
