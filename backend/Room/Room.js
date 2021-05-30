0c45186455328448c147ee5f8279ab766d56362f
Manoj Singh Negi <justanothermanoj@gmail.com>
2021-05-30 18:30:53 +0530

    added rooms

---
+++ backend/Room/Room.js
@@ -0,0 +1,28 @@
+const { v4 } = require("uuid");
+
+class Room {
+    participants = []
+    roomId = null
+    author = null
+
+    constructor (author) {
+        this.roomId = v4();
+        this.author = author
+    }
+
+    addParticipant = (participantId) => {
+        this.participants.push(participantId)
+    }
+
+    removeParticipant = (participantId) => {
+        let index = this.participants.findIndex(
+            (existingParticipantId) => existingParticipantId === participantId
+        )
+
+        if (index > -1) {
+            this.participants.splice(index, 1)
+        }
+    }
+}
+
+module.exports = Room
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const express = require('express');
+const { json } = require('body-parser');
 const { startPeerServer } = require('./peer/server');
+const Room = require('./Room/Room');
 
 const app = express();
+
+app.use(json())
 
 app.get('/', (req, res, next) => res.send('Hello world!'));
 
@@ -10,6 +14,27 @@ const server = http.createServer(app);
 
 server.listen(8000);
 
-startPeerServer();
-
+// will consist of all our participants
 const rooms = []
+
+app.post('/rooms', (req, res) => {
+    const { body } = req;
+
+    const newRoom = new Room(body.author);
+
+    rooms.push(newRoom)
+
+    res.json({
+        roomId: newRoom.roomId
+    })
+})
+
+app.get('/rooms/:roomId', (req, res) => {
+    const { params } = req;
+
+    const room = rooms.find(existingRooms => existingRooms.roomId === params.roomId);
+
+    res.json({ ...room })
+})
+
+startPeerServer();
